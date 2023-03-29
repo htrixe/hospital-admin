00e98698c7ae6b385da346d8f021f69008fe5250
humendes <hugo-manuel.mendes@altran.com>
2023-03-29 20:22:51 +0100

    lazy loading

--- admin-pro-backend/index.js
+++ admin-pro-backend/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
@@ -17,6 +18,9 @@ app.use( express.json() );
 // Base de datos
 dbConnection();
 
+// Directorio público
+app.use( express.static('public') );
+
 // Rotas
 app.use( '/api/usuarios', require('./routes/usuarios') );
 app.use( '/api/hospitales', require('./routes/hospitales') );
@@ -25,6 +29,10 @@ app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/login', require('./routes/auth') );
 app.use( '/api/upload', require('./routes/uploads') );
 
+// Lo último
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html' ) );
+});
 
 
 app.listen( process.env.PORT, () => {
--- front-adminpro/src/environments/environment.prod.ts
+++ front-adminpro/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
-
 export const environment = {
   production: true,
-  base_url: 'http://localhost:3000/api'
+  base_url: 'https://adminpro-backend.herokuapp.com/api'
 };
